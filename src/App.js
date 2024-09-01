66028ecf8342c33dec20a02f17530a38c2d7c625
André Teixeira <1101153@isep.ipp.pt>
2024-09-01 18:05:51 +0100

    New init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Minha Página Simples</h1>
+        <p>
+          Bem-vindo à minha primeira aplicação React!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
