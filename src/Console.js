95d5e428fc79b009b79c798518ece782d9ba37ec
André Teixeira <1101153@isep.ipp.pt>
2024-09-01 21:26:11 +0100

    First animation

---
+++ src/Console.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Console.css';
+
+function Console() {
+  return (
+    <div className="ConsoleContainer">
+      <div className="console"></div>
+    </div>
+  );
+}
+
+export default Console;
